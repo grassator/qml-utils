89126ec14b875e745e07a82f88396aa5a92d2ec9
Dmitriy Kubyshkin <dmitriy@kubyshkin.ru>
2014-02-05 10:12:14 +0700

    Added platform information. Better directory structure

---
+++ native/platforminformation.h
@@ -0,0 +1,39 @@
+#ifndef PLATFORMINFORMATION_H
+#define PLATFORMINFORMATION_H
+
+#include <QObject>
+#include <QtQuick>
+
+class PlatformInformation : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY (QString name READ name)
+public:
+    explicit PlatformInformation(QObject *parent = 0)
+        : QObject(parent), m_platformName("Windows")
+    {
+#ifdef Q_OS_OSX
+        m_platformName = "OSX";
+#endif
+
+#ifdef Q_OS_UNIX
+        m_platformName = "Linux";
+#endif
+    }
+
+    QString name() const { return m_platformName; }
+
+private:
+    QString m_platformName;
+
+};
+
+static QObject* platformInformationProvider(QQmlEngine *engine, QJSEngine *scriptEngine)
+{
+    Q_UNUSED(engine)
+    Q_UNUSED(scriptEngine)
+
+    return new PlatformInformation();
+}
+
+#endif // PLATFORMINFORMATION_H
