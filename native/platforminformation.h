03165c116bb7af96d7854f04216907be7ae0605a
Dmitriy Kubyshkin <dmitriy@kubyshkin.ru>
2014-02-10 10:49:47 +0700

    Fixed issue with OSX detected as linux

--- native/platforminformation.h
+++ native/platforminformation.h
@@ -18,26 +18,36 @@ public:
 
 #ifdef Q_OS_OSX
         m_platformName = "OSX";
-#endif
-
+#else
 #ifdef Q_OS_UNIX
         m_platformName = "Linux";
+#endif
 #endif
     }
 
     QString name() const { return m_platformName; }
 
+    static void registerQmlSingleton(const char *uri = "Native", int majorVersion = 1, int minorVersion = 0) {
+        // @uri Native
+        qmlRegisterSingletonType<PlatformInformation>(uri,
+                                                      majorVersion,
+                                                      minorVersion,
+                                                      "Platform",
+                                                      platformInformationProvider);
+    }
+
 private:
     QString m_platformName;
 
+    static QObject* platformInformationProvider(QQmlEngine *engine, QJSEngine *scriptEngine)
+    {
+        Q_UNUSED(engine)
+        Q_UNUSED(scriptEngine)
+        return new PlatformInformation();
+    }
+
 };
 
-static QObject* platformInformationProvider(QQmlEngine *engine, QJSEngine *scriptEngine)
-{
-    Q_UNUSED(engine)
-    Q_UNUSED(scriptEngine)
 
-    return new PlatformInformation();
-}
 
 #endif // PLATFORMINFORMATION_H
